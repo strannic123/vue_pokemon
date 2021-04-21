415856330d303aa754e8c1fdd228b46be872d676
strannic123 <manager350@mail.ru>
2021-04-21 14:56:07 +0300

    add evolutions

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cards">
-    <Card v-for="starter in starters">
+    <Card v-for="starter in starters" @click="fetchEvolutions(starter)">
 
       <template v-slot:title>
         {{starter.name}}
@@ -19,13 +19,33 @@
     </Card>
   </div>
 
+  <div class="cards">
+    <Card v-for="evolut in evolutions">
+
+      <template v-slot:title>
+        {{evolut.name}}
+      </template>
+
+      <template v-slot:content>
+        <img :src="evolut.sprites" alt="{{evolut.name}}">
+      </template>
+
+      <template v-slot:description>
+        <div v-for="elem in evolut.type">
+          {{elem}}
+        </div>
+      </template>
+
+    </Card>
+  </div>
+
 </template>
 
 <script>
 import Card from './Card'
 
 const baseUrl = 'https://pokeapi.co/api/v2'
-const ids = [1, 4, 7]
+const STARTER_IDS = [1, 4, 7]
 export default {
   name: "Cards",
   components: {
@@ -33,22 +53,27 @@ export default {
   },
   data(){
     return {
-      starters: []
+      starters: [],
+      evolutions: []
     }
   },
   methods: {
-   async fetchData(){
+    async fetchEvolutions(pokemon){
+      this.evolutions = await this.fetchData([pokemon.id+1, pokemon.id+2])
+    },
+   async fetchData(ids){
      const responses = await Promise.all(ids.map(id =>window.fetch(`${baseUrl}/pokemon/${id}`)))
      const data = await Promise.all(responses.map(res => res.json()))
-     this.starters = data.map(datum => ({
-         name: datum.name,
-         sprites: datum.sprites.other['official-artwork'].front_default,
-         type: datum.types.map(type =>  type.type.name)
+      return data.map(datum => ({
+       id: datum.id,
+       name: datum.name,
+       sprites: datum.sprites.other['official-artwork'].front_default,
+       type: datum.types.map(type =>  type.type.name)
      }))
    }
   },
-  created() {
-    this.fetchData()
+  async created() {
+    this.starters = await this.fetchData(STARTER_IDS)
   }
 
 }
